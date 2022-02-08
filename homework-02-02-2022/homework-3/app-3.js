f5ab89743b476368c6e34474ab2dbde180cbf71a
Stoian-Iulia <71259681+Stoian-Iulia@users.noreply.github.com>
2022-02-08 19:38:20 +0200

    Add files via upload

--- homework-02-02-2022/homework-3/app-3.js
+++ homework-02-02-2022/homework-3/app-3.js
@@ -5,5 +5,5 @@ const digit3 = Number(prompt("Input third digit"));
 let res1 = Math.max(digit1, digit2, digit3);
 let res2 = Math.min(digit1, digit2, digit3);
 
-alert(`Наибольшее число: ${res1}`);
-alert(`Наименьшее число: ${res2}`);
+alert(`The largest number: ${res1}`);
+alert(`The smallest number: ${res2}`);
