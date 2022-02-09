576fc71aa3083adb9b840630560113178527f00b
Stoian-Iulia <71259681+Stoian-Iulia@users.noreply.github.com>
2022-02-09 10:03:39 +0200

    Add files via upload

---
+++ homework-02-02-2022/homework-6/app-6.js
@@ -0,0 +1,10 @@
+const number = prompt('Enter a number', 0);
+
+if (number > 0) {
+  alert( 1 );
+} else if (number < 0) {
+  alert( -1 );
+} else if (number === isNaN && number === 0 ) {
+  alert( 0 );
+} else alert('Error');
+  
