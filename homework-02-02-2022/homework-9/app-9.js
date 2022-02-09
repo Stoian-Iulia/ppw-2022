d6523b5cc3b64b1005bb5d877a4af7cbade1b926
Stoian-Iulia <71259681+Stoian-Iulia@users.noreply.github.com>
2022-02-09 09:20:09 +0200

    Add files via upload

---
+++ homework-02-02-2022/homework-9/app-9.js
@@ -0,0 +1,9 @@
+alert("Hello!");
+const userName = prompt("What is your name");
+alert(`Hello, ${userName}!`);
+const likeJS = confirm("Did you know about JS?");
+if(likeJS){
+    alert("Great!");
+} else {
+    alert("You can start learning right now");
+}
