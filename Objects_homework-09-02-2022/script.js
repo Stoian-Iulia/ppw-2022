2ee3b9b4225d37128470a10c2e00b80eae46593b
Julia <71259681+Stoian-Iulia@users.noreply.github.com>
2022-02-24 17:09:12 +0200

    Add files via upload

--- Objects_homework-09-02-2022/script.js
+++ Objects_homework-09-02-2022/script.js
@@ -54,3 +54,24 @@ var menu = {
   multiplyNumeric(menu);
   
   console.log( menu.width, menu.height, menu.title );
+
+
+//Задание 4.
+console.log("Задание 4");
+let user1 = {
+	name: 'User 1',
+	age: 123
+};
+
+let user2 = {};
+for (let key in user1)
+{
+	user2[key] = user1[key];
+}
+user2.name = "User 2";
+user2.age = 321;
+console.log(user1);
+console.log(user2);
+
+
+
