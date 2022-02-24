0dcd085daf36d02dc32f787a528488f16d8ae19a
Julia <71259681+Stoian-Iulia@users.noreply.github.com>
2022-02-24 18:18:14 +0200

    Add files via upload

--- Objects_homework-09-02-2022/script.js
+++ Objects_homework-09-02-2022/script.js
@@ -74,4 +74,27 @@ console.log(user1);
 console.log(user2);
 
 
+//Задание 5.
+console.log("Задание 5");
+let object = {
+    name: "Person",
+	age: 123,
+	salary: 1542.33,
+	contacts: {
+		phone: "112",
+		email: "email@domain.com"
+    },
+    address: "Moldova"
+}
+object.contacts.phone = "+37312345678";
+delete object.address;
+console.log(object);
+
+
+//Задание 6.
+console.log("Задание 6");
+
+
+
+
 
